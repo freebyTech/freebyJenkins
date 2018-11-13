a1812e0f142e19456fdf8ef773a8a3fea39caa6a
James Eby <temporafugiunt@gmail.com>
2018-11-13 11:09:55 -0600

    Adding logging.

--- docker/jenkins-scripts/addEnvVarsAsGlobalProperties.groovy
+++ docker/jenkins-scripts/addEnvVarsAsGlobalProperties.groovy
@@ -11,8 +11,10 @@ def envVars = null
 if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
   newEnvVarsNodeProperty = new EnvironmentVariablesNodeProperty();
   globalNodeProperties.add(newEnvVarsNodeProperty)
+  printLn 'No environment variable node properties existed, creating new.'
   envVars = newEnvVarsNodeProperty.getEnvVars()
 } else {
+  printLn 'Environment variable node properties exist, adding to list.'
   envVars = envVarsNodePropertyList.get(0).getEnvVars()
 }
 
@@ -20,6 +22,7 @@ def val = System.getenv('AD_DOMAIN')
 
 // Null or empty check.
 if(val?.trim()) {
+  printLn 'Adding AD_DOMAIN'
   envVars.put('AD_DOMAIN', val)
 }
 
@@ -27,6 +30,7 @@ val = System.getenv('REGISTRY_URL')
 
 // Null or empty check.
 if(val?.trim()) {
+  printLn 'Adding REGISTRY_URL'
   envVars.put('REGISTRY_URL', val)
 }
 
@@ -34,6 +38,7 @@ val = System.getenv('GIT_REPO_URL')
 
 // Null or empty check.
 if(val?.trim()) {
+  printLn 'Adding GIT_REPO_URL'
   envVars.put('GIT_REPO_URL', val)
 }
 
